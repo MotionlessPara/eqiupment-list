b02d9644c0dfea9d7f2e93f27b8e5ee6f27c26fe
MotionlessPeri <839160689@qq.com>
2018-02-12 04:44:03 +0800

    server.js update

--- server/server.js
+++ server/server.js
@@ -8,27 +8,30 @@ let connection = mysql.createConnection({
 	port: "3306",
 	database: "EquipmentList"
 });
+connection.connect();
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1')
-    if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+    if(req.method=="OPTIONS") res.send(200);
     else  next();
 });
 app.post("/equipment", (req, res) => {
-	console.log("POST", "/equipment", req.query, req.body);
-	// connection.connect();
-	let sql = "insert into Equipment values()";
-	res.send("lalala");
-	res.end();
-	// connection.query(sql , (err, result) => {
-	// 	if(err) {
-	// 		return;
-	// 	}
-	// 	console.log(result);
-	// 	res.send(result);
-	// });
+	console.log("POST", "/equipment", req.query, req.body, req.params);
+	let sql = "insert into Equipment values(?,?,?,?)";
+	let sqlParam = [req.query.name, req.query.count, req.query.describe, req.query.count];
+	// res.send("lalala");
+	console.log("POST", "/equipment", sql, sqlParam);
+	connection.query(sql, sqlParam, (err, result) => {
+		if(err) {
+			console.log(err);
+			res.send(err);
+			return;
+		}
+		console.log(result);
+		res.send(result);
+	});
 })
 
 app.delete("/equipment", (req, res) => {
