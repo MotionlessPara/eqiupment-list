f0b6837eebb191d72d5e9205c287a2b4181fcf0f
MotionlessPara <839160689@qq.com>
2018-02-12 02:05:08 +0800

    add readme.md

--- server/server.js
+++ server/server.js
@@ -1,11 +1,35 @@
-import Express from 'express';
-import Mysql from 'mysql';
-let app = Express();
-let connection = Mysql.createConnection({
+let express = require('express');
+let mysql = require('mysql');
+let app = express();
+let connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "xiedong",
 	port: "3306",
-	database: "Equipment"
+	database: "EquipmentList"
 });
-app.get("/", )
+app.post("/equipment", (req, res) => {
+	console.log("POST", "/equipment", req.query);
+	// connection.connect();
+	console.log(res);
+	// let sql = "insert into Equipment values()"
+	// connection.query(sql , (err, result) => {
+	// 	if(err) {
+	// 		return;
+	// 	}
+	// 	console.log(result);
+	// 	res.send(result);
+	// });
+})
+
+// app.delete("/equipment", (req, res) => {
+// 	connection.connect();
+// 	connection.query(sql, (err, result) => {
+// 		if(err) {
+// 			return;
+// 		}
+// 		console.log(result);
+// 	})
+// })
+// 
+let server = app.listen(8000, () => {console.log("server start")});
